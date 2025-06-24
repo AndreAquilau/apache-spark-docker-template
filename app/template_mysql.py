374da3a70abc8cadc97439dba5b5a7dd3b7c76ba
andre.aquilau <andre.aquilau@outlook.com>
2025-06-24 04:25:06 -0400

    Exemplos de Transformação de Dados e Joins

--- app/template_mysql.py
+++ app/template_mysql.py
@@ -1,4 +1,5 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import upper, trim
 
 spark = SparkSession.builder \
     .appName("MySQL JDBC Example") \
@@ -19,3 +20,52 @@ df = spark.read.format("jdbc") \
     .load()
 
 df.show()
+
+sql = "(SELECT * FROM cliente WHERE nome = 'Ana Silva') AS clientes_ana"
+
+df = spark.read.format("jdbc") \
+    .option("url", url) \
+    .option("driver", "com.mysql.cj.jdbc.Driver") \
+    .option("user", usuario) \
+    .option("password", senha) \
+    .option("dbtable", sql) \
+    .load()
+
+df_transformado = df.withColumn('nome', upper(trim(df['nome'])))
+df_transformado.show()
+
+
+# Primeira consulta
+sql1 = "(SELECT * FROM cliente WHERE nome = 'Ana Silva') AS clientes_sp"
+df1 = spark.read.format("jdbc") \
+    .option("url", url) \
+    .option("driver", "com.mysql.cj.jdbc.Driver") \
+    .option("user", usuario) \
+    .option("password", senha) \
+    .option("dbtable", sql1) \
+    .load()
+
+# Segunda consulta
+sql2 = "(SELECT * FROM cliente WHERE nome = 'Ana Silva') AS clientes_rj"
+df2 = spark.read.format("jdbc") \
+    .option("url", url) \
+    .option("driver", "com.mysql.cj.jdbc.Driver") \
+    .option("user", usuario) \
+    .option("password", senha) \
+    .option("dbtable", sql2) \
+    .load()
+
+
+# INNER JOIN
+df_join = df1.join(df2, df1.id == df2.id, 'inner')
+
+# LEFT JOIN
+df_join = df1.join(df2, df1.id == df2.id, 'left')
+
+# RIGHT JOIN
+df_join = df1.join(df2, df1.id == df2.id, 'right')
+
+# FULL OUTER JOIN
+df_join = df1.join(df2, df1.id == df2.id, 'outer')
+
+df_join.show()
